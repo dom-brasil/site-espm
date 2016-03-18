14f67e892219ea7ef6a39b171762945832e5b541
Leandro Kenji <locky.kenji@gmail.com>
2016-03-18 12:24:57 -0300

    /js

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(function(){
+
+	$('#menu-menu-1').dropit({
+		action: 'mouseenter'
+	});
+
+	$('.play').hover(
+		function(){
+			$('.play img').animate({
+				width: '98px'
+			}, 200);
+		}, 
+		function(){
+			$('.play img').animate({
+				width: '90px'
+			}, 200);
+		}
+	);
+
+	$('.play').on('click', function(){
+
+		$('.play').hide();
+
+		$('.video_container').html('<iframe width="639" height="360" src="//www.youtube.com/embed/lLrPZR_7w5I?rel=0&autoplay=true" frameborder="0" allowfullscreen></iframe>');
+	})
+
+})();
